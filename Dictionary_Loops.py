0f68bdd5e1bf11bc125034ab6df4d65c01367810
TDBJR <Thomasdburner@gmail.com>
2017-01-10 21:20:09 -0800

    Update Dictionary_Loops.py

--- Dictionary_Loops.py
+++ Dictionary_Loops.py
@@ -1,4 +1,4 @@
-#This shit is clever  since the loop only sees the the name and not the value printing a dict in a loop will only list the names
+#Since the loop only sees the the name and not the value printing a dict in a loop will only list the names
 # so if you want it to list the value instead you type the name of the dict and the first variable of the for loop  inside square brakets
 # the_links[keys]  since keys is going to be the name it really means   the_list['a'] and cylcles through all the names thus printing 
 #all the values
